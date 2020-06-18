5bdbc9c40be7aa3797b364d194862b2264daf465
Nicholas Nam <admin@nicholasnam.com>
2020-06-18 10:25:26 -0400

    Tables Created

--- migrations/20200618141414-create-recipes-to-categories.js
+++ migrations/20200618141414-create-recipes-to-categories.js
@@ -2,22 +2,32 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('RecipesCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      recipesId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Recipes',
+          key: 'id'
+        },
+        categoriesId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Recipes',
+            key: 'id'
+          },
+      }
+    
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('RecipesCategories');
   }
 };
